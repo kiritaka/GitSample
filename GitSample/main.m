516315a0adddeeac07d32b0cca396286d513ac1f
桐村　尚志 <tkirimur@MBA-11Z-248.local>
2014-03-19 17:25:49 +0900

    Initial Commit

---
+++ GitSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GitSample
+//
+//  Created by 桐村　尚志 on 2014/03/19.
+//  Copyright (c) 2014年 桐村　尚志. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
